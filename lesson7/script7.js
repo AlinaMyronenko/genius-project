ee7f88acdbe9be893d982f169eaed5ea4cc80f69
alina myronenko <alinamyronenko93@gmail.com>
2023-05-19 11:00:20 +0300

    lesson8

--- lesson7/script7.js
+++ lesson7/script7.js
@@ -5,37 +5,37 @@
 // sum() повертає суму цих властивостей.
 // mul() повертає результат множення даних властивостей.
 
-class Calculator{
-    constructor(props) {
-     this.read = props.read
-    }
-    x = 0;
-    y = 0;
+// class Calculator{
+//     constructor(props) {
+//      this.read = props.read
+//     }
+//     x = 0;
+//     y = 0;
 
-     set read(information) {
-        const number = information.split(',');
-        this.x = number[0];
-        this.y = number[1];
-    }
+//      set read(information) {
+//         const number = information.split(',');
+//         this.x = number[0];
+//         this.y = number[1];
+//     }
 
-    sum() { 
-        if (isNaN(this.x) || isNaN(this.y)) {
-            return alert('Не вірно введені значення')
-        }
-       return alert(+this.x + +this.y)
-    }
+//     sum() { 
+//         if (isNaN(this.x) || isNaN(this.y)) {
+//             return alert('Не вірно введені значення')
+//         }
+//        return alert(+this.x + +this.y)
+//     }
     
-    mul() {
-         if (isNaN(this.x) || isNaN(this.y)) {
-            return alert('Не вірно введені значення')
-        }
-       return alert(this.x * this.y )
-    }
-}
+//     mul() {
+//          if (isNaN(this.x) || isNaN(this.y)) {
+//             return alert('Не вірно введені значення')
+//         }
+//        return alert(this.x * this.y )
+//     }
+// }
 
-const result = new Calculator({
-    read: prompt('Введіть два числа, через кому.')
-})
+// const result = new Calculator({
+//     read: prompt('Введіть два числа, через кому.')
+// })
 
-result.sum();
-result.mul();
+// result.sum();
+// result.mul();
