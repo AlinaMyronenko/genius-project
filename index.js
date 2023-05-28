18f513644af7b2f62fd2cccc658355bfdd486e88
alina myronenko <alinamyronenko93@gmail.com>
2023-05-28 20:27:20 +0300

    lesson11

---
+++ index.js
@@ -0,0 +1,8 @@
+// import './style.css';
+// import isEmail from 'validator/lib/isEmail';
+
+// console.log(isEmail('foo@bar.com'));
+
+var moment = require('moment');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
